04f850708acd34362c22916dd3e3f210f6ccafd2
Mark Hoggatt <mark@hoggatt.eu>
2018-01-24 22:19:12 +0000

    Create preferences view and token field.

---
+++ MacAppCentre/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsViewController.swift
+//  MacAppCentre
+//
+//  Created by Mark Hoggatt on 24/01/2018.
+//  Copyright © 2018 Mark Hoggatt. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsViewController : NSViewController
+{
+	@IBOutlet weak var privateTokenField: NSSecureTextField!
+	override func viewDidLoad()
+	{
+	}
+
+	override var representedObject: Any?
+	{
+		didSet
+		{
+
+		}
+	}
+}
