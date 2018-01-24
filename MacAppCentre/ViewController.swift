57741677f55bb3edd01b0ef3b5bc4f2b17ce5c55
Mark Hoggatt <mark@hoggatt.eu>
2018-01-24 21:41:52 +0000

    Initial Commit

---
+++ MacAppCentre/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacAppCentre
+//
+//  Created by Mark Hoggatt on 24/01/2018.
+//  Copyright © 2018 Mark Hoggatt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
